470257698d674363c547a729b9faf86906dc2408
menorziin <williamsilvapppl@gmail.com>
2022-12-14 17:44:42 -0300

    Revert "Battery Styles: Fix signal icon padding with Hidden style"
    This reverts commit f966abb7ae5ebb1785cae8f7fd9a26ea93788400.

--- packages/SystemUI/src/com/android/systemui/battery/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/battery/BatteryMeterView.java
@@ -98,8 +98,6 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
 
     private DualToneHandler mDualToneHandler;
 
-    private final ArrayList<BatteryMeterViewCallbacks> mCallbacks = new ArrayList<>();
-
     private int mNonAdaptedSingleToneColor;
     private int mNonAdaptedForegroundColor;
     private int mNonAdaptedBackgroundColor;
@@ -410,9 +408,6 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
             mBatteryIconView.setVisibility(View.VISIBLE);
             scaleBatteryMeterViews();
         }
-        for (int i = 0; i < mCallbacks.size(); i++) {
-            mCallbacks.get(i).onHiddenBattery(mBatteryStyle == BATTERY_STYLE_HIDDEN);
-        }
     }
 
     private Drawable getUnknownStateDrawable() {
@@ -551,18 +546,6 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
     public interface BatteryEstimateFetcher {
         void fetchBatteryTimeRemainingEstimate(
                 BatteryController.EstimateFetchCompletion completion);
-    }
-
-    public interface BatteryMeterViewCallbacks {
-        default void onHiddenBattery(boolean hidden) {}
-    }
-
-    public void addCallback(BatteryMeterViewCallbacks callback) {
-        mCallbacks.add(callback);
-    }
-
-    public void removeCallback(BatteryMeterViewCallbacks callbacks) {
-        mCallbacks.remove(callbacks);
     }
 }
 
