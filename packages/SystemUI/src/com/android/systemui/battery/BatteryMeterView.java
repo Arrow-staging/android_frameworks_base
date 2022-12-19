51fa416e229c2879174bfcaab4ee701be44357dd
menorziin <williamsilvapppl@gmail.com>
2022-12-19 14:18:39 -0300

    Revert "Battery Styles: Show a bolt ⚡ when charging"
    This reverts commit dd0627cbdd00743d5c3d9ef5e9d22d3089f00ce2.

--- packages/SystemUI/src/com/android/systemui/battery/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/battery/BatteryMeterView.java
@@ -244,7 +244,6 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
             mThemedDrawable.setBatteryLevel(mLevel);
             mCircleDrawable.setBatteryLevel(mLevel);
             mFullCircleDrawable.setBatteryLevel(mLevel);
-            updatePercentText();
         }
         if (mCharging != pluggedIn) {
             mCharging = pluggedIn;
@@ -252,6 +251,7 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
             mCircleDrawable.setCharging(mCharging);
             mFullCircleDrawable.setCharging(mCharging);
             updateShowPercent();
+        } else {
             updatePercentText();
         }
     }
@@ -328,13 +328,6 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
             mBatteryPercentView.setText(percentText);
         }
 
-        // Use the high voltage symbol ⚡ (u26A1 unicode) but prevent the system
-        // to load its emoji colored variant with the uFE0E flag
-        String bolt = "\u26A1\uFE0E";
-        CharSequence mChargeIndicator = mCharging && (mBatteryStyle == BATTERY_STYLE_HIDDEN ||
-            mBatteryStyle == BATTERY_STYLE_TEXT) ? (bolt + " ") : "";
-        mBatteryPercentView.setText(mChargeIndicator +
-            NumberFormat.getPercentInstance().format(mLevel / 100f));
         setContentDescription(
                 getContext().getString(mCharging ? R.string.accessibility_battery_level_charging
                         : R.string.accessibility_battery_level, mLevel));
