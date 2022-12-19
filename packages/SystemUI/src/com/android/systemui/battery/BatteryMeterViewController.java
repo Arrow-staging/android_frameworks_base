e28d1b7c49cd1c914ba69e06f1d32579f2a08af5
menorziin <williamsilvapppl@gmail.com>
2022-12-19 14:39:04 -0300

    Revert "SystemUI: Bring back good ol' circle battery style"
    This reverts commit 506fa4d9dd01a88864569cd96c856d2698339afb.
    
    Change-Id: I50227e97a9bbcbc8cba06d969a87ead8d0fe3dcc

--- packages/SystemUI/src/com/android/systemui/battery/BatteryMeterViewController.java
+++ packages/SystemUI/src/com/android/systemui/battery/BatteryMeterViewController.java
@@ -15,7 +15,7 @@
  */
 package com.android.systemui.battery;
 
-import static android.provider.Settings.System.STATUS_BAR_SHOW_BATTERY_PERCENT;
+import static android.provider.Settings.System.SHOW_BATTERY_PERCENT;
 
 import android.app.ActivityManager;
 import android.content.ContentResolver;
@@ -63,13 +63,7 @@ public class BatteryMeterViewController extends ViewController<BatteryMeterView>
             if (StatusBarIconController.ICON_HIDE_LIST.equals(key)) {
                 ArraySet<String> icons = StatusBarIconController.getIconHideList(
                         getContext(), newValue);
-                mBatteryHidden = icons.contains(mSlotBattery);
-                mView.setVisibility(mBatteryHidden ? View.GONE : View.VISIBLE);
-            } else if (BatteryMeterView.STATUS_BAR_BATTERY_STYLE.equals(key)) {
-                if (!mBatteryHidden) {
-                    mView.setBatteryStyle(TunerService.parseInteger(newValue,
-                            BatteryMeterView.BATTERY_STYLE_PORTRAIT));
-                }
+                mView.setVisibility(icons.contains(mSlotBattery) ? View.GONE : View.VISIBLE);
             }
         }
     };
@@ -95,8 +89,6 @@ public class BatteryMeterViewController extends ViewController<BatteryMeterView>
     // Some places may need to show the battery conditionally, and not obey the tuner
     private boolean mIgnoreTunerUpdates;
     private boolean mIsSubscribedForTunerUpdates;
-
-    private boolean mBatteryHidden;
 
     @Inject
     public BatteryMeterViewController(
@@ -164,8 +156,7 @@ public class BatteryMeterViewController extends ViewController<BatteryMeterView>
             return;
         }
 
-        mTunerService.addTunable(mTunable, StatusBarIconController.ICON_HIDE_LIST,
-                BatteryMeterView.STATUS_BAR_BATTERY_STYLE);
+        mTunerService.addTunable(mTunable, StatusBarIconController.ICON_HIDE_LIST);
         mIsSubscribedForTunerUpdates = true;
     }
 
@@ -180,7 +171,7 @@ public class BatteryMeterViewController extends ViewController<BatteryMeterView>
 
     private void registerShowBatteryPercentObserver(int user) {
         mContentResolver.registerContentObserver(
-                Settings.System.getUriFor(STATUS_BAR_SHOW_BATTERY_PERCENT),
+                Settings.System.getUriFor(SHOW_BATTERY_PERCENT),
                 false,
                 mSettingObserver,
                 user);
